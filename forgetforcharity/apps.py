317712ce29156ee4d662573d40ca9e9dadba1a7a
Michael Manis <electricpyrotechnic@gmail.com>
2017-09-02 18:23:36 -0400

    Initial commit

---
+++ forgetforcharity/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ForgetForCharityConfig(AppConfig):
+    name = 'forgetforcharity'
---
+++ forgetforcharity/static/forgetforcharity/js/index.js
@@ -0,0 +1,26 @@
+var button_pos;
+var nav_bar;
+
+$(document).ready(function () {
+    var leaves = $(".svg-leaf");
+    var sign_in = $('#sign-in');
+    nav_bar = $('#nav-bar');
+    button_pos = sign_in.offset().top + sign_in.outerHeight(true);
+
+    TweenMax.to(leaves, 0, {scale: 0.1});
+    TweenMax.to(leaves, 0.3, {scale: 1.5, opacity: 1, delay: 0.5, ease: Power1.easeOut});
+    TweenMax.to(leaves, 0.1, {scale: 1, delay: 0.8});
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > button_pos) {
+            nav_bar.css('display', 'flex');
+            TweenMax.to(nav_bar, 0.5, {opacity: 1});
+        } else {
+            TweenMax.to(nav_bar, 0.5, {
+                opacity: 0, onComplete: function () {
+                    nav_bar.css('display', 'none');
+                }
+            });
+        }
+    });
+});
